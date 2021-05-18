6d429a39eeeaf217be9182df42da7a0a9219766e
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2021-05-17 20:29:48 -0500

    Add files via upload

---
+++ Vet.cpp
@@ -0,0 +1,30 @@
+/*
+Vet
+Name
+Date
+This program uses a class to create Cat objects representing
+patients at the vet. While Mr. Pawelski was able to write
+the class, he did not have enough time to create objects.
+Using the class, create three objects with the values outlined
+in the assignment document stored into the attributes.
+*/
+#include <iostream>
+using namespace std;
+
+/*
+Represents a cat with the given name, age, weight, and color.
+*/
+class Cat
+{
+public:
+    string name;
+    int age;
+    double weight;
+    string color;
+};
+
+int main()
+{
+    // Add your code here!
+}
+
